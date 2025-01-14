9a34895829beb11f92b313aba81ecc23b2ec4bd7
Shaker <aishaker129@gmail.com>
2025-01-14 23:55:53 +0600

    add folder

---
+++ DOM/signup.js
@@ -0,0 +1,9 @@
+const list = [];
+const user = new Map();
+document.getElementById("submit").addEventListener("click", myfunction);
+
+function myfunction() {
+    
+}
+
+document.getElementById("submit").innerHTML()
